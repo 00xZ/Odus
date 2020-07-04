cfd6ab8bab0843160d17c3d75c66e1d5fbe5d0d3
Svnr <eyezikbra@gmail.com>
2020-07-04 15:05:07 -0400

    Create scan4http.py

---
+++ scan4http.py
@@ -0,0 +1,26 @@
+import sys 
+import random  
+import pickle
+print "\n +-+-+-+==+-+-+-+=free shmurda=+-+-+-+==+-+-+-+"
+import requests
+import threading
+import os, re, time, socket
+port = 80
+def scanner():
+	ip = str(random.randrange(0,256))+'.'+str(random.randrange(0,256))+'.'+str(random.randrange(0,256))+'.'+str(random.randrange(0,256))
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.settimeout(1.5)
+	result = sock.connect_ex((ip, port))
+	sock.close()
+	if result == 0:
+		print("OPEN : " + ip)
+		kkk = open("list.lst", "a").write(ip + "\n")
+	else:
+		pass
+for x in range(0,10): #prolly didnt even do this right but fuck it it will be updated when i have time
+	try:
+		t = threading.Thread(target=scanner())
+		threads.append(t)
+		t.start()
+	except:
+		pass
